e28dea68c1e55a0f7ab4033a0929330a3b1519b3
szt3molcsa <szt3molcsa@vasvari.org>
2024-12-05 13:46:25 +0100

    somehow works

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ const startServer = async () => {
     await initializeDB();
     app.listen(3000, () => console.log("Server is running on port 3000"));
 };
-
+app.use("/api/users", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 startServer();
