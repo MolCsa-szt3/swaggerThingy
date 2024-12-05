bbcb184c6648cb48cfc66e8f98fb5ed4ac8ef134
szt3molcsa <szt3molcsa@vasvari.org>
2024-12-05 13:51:15 +0100

    goostbasterrs

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,15 @@ router.get("/", async (req,res,next)=> {
 })
 
 
+router.get("/:id", async (req, res, next) => {
+    try {
+        const [user] = await dbQuery("SELECT * FROM users WHERE id = ?;", [req.params.id]);
+        if (!user) return res.status(404).json({ message: "User not found" });
+        res.status(200).json(user);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
 export default router;
