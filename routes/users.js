8a82aa7c6590fe8c0f1bc5df0fcba40539602174
szt3molcsa <szt3molcsa@vasvari.org>
2024-12-05 13:11:18 +0100

    users get all

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ import { dbQuery, dbRun } from "../database.js";
 
 const router = express.Router();
 
-
+router.get("/", async (req,es,next)=> {
+    try {
+        const users = await dbQuery("SELECT * FROM users;")
+        res.status(200).json(users)
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 export default router;
